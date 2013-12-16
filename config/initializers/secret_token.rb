33d55d1f960f382d7fbc93a353ce105f7ec757f5
calvinhbj <bingjiu.he@163.com>
2013-12-16 14:48:34 +0800

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rdrcs::Application.config.secret_key_base = '99359e395241c3c7f3047d0cf139b8fae0df7ccf06fb4f9a5eac78e5a5d222fc81ae85f1d1e5579ee6111db9599408ac23dc42c184ab0f0ea8f66540b3641bea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rdrcs::Application.config.session_store :cookie_store, key: '_rdrcs_session'
